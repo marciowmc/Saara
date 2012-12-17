0df63c59bec0fb95430f273a0f9ec56bf2d321f1
Marcio William <marcio@easytaxi.com.br>
2012-12-17 14:35:01 -0200

    Ajustado as coordenadas...

--- src/br/com/saara/LojasActivity.java
+++ src/br/com/saara/LojasActivity.java
@@ -36,34 +36,34 @@ public class LojasActivity extends Activity {
 		
 		switch (id_categoria) {
 		case 2:
-			listLojas.add(new Lojas("ARMARINHO DELMAR","Loja Tradicional de Brinquedos, com mais de 100 anos.","R. Sr. dos Passos, 279/283",-43.22,-23.22,1));
-			listLojas.add(new Lojas("BRINK MANIA","Loja Tradicional de Brinquedos, com mais de 200 anos.","R. Sr. dos Passos, 279/283",-43.22,-23.22,2));
-			listLojas.add(new Lojas("LOJAS SIMÕES","Loja Tradicional de Brinquedos, com mais de 300 anos.","R. Sr. dos Passos, 279/283",-43.22,-23.22,3));
-			listLojas.add(new Lojas("FESTA DA CIDADE - PRESENTES","Loja Tradicional de Brinquedos, com mais de 400 anos.","R. Sr. dos Passos, 279/283",-43.22,-23.22,4));
+			listLojas.add(new Lojas("ARMARINHO DELMAR","Loja Tradicional de Brinquedos, com mais de 100 anos.","R. Sr. dos Passos, 279/283",-22.905204,-43.186828,1));
+			listLojas.add(new Lojas("BRINK MANIA","Loja Tradicional de Brinquedos, com mais de 200 anos.","R. da Alfândega, 314",-22.905204,-43.186828,2));
+			listLojas.add(new Lojas("LOJAS SIMÕES","Loja Tradicional de Brinquedos, com mais de 300 anos.","R. da Alfândega, 240",-22.904127,-43.185734,3));
+			listLojas.add(new Lojas("FESTA DA CIDADE - PRESENTES","Loja Tradicional de Brinquedos, com mais de 400 anos.","R. da Alfândega, 240",-22.904127,-43.185734,4));
 			break;
 
 		case 1 :
 			
-			listLojas.add(new Lojas("ALFA 167","Loja Tradicional de Brinquedos, com mais de 100 anos.","R. Sr. dos Passos, 279/283",-43.22,-23.22,1));
-			listLojas.add(new Lojas("HOT FASHION 373","Loja Tradicional de Brinquedos, com mais de 200 anos.","R. Sr. dos Passos, 279/283",-43.22,-23.22,2));
-			listLojas.add(new Lojas("268 BIJUTERIAS","Loja Tradicional de Brinquedos, com mais de 300 anos.","R. Sr. dos Passos, 279/283",-43.22,-23.22,3));
-			listLojas.add(new Lojas("CAÇULA BIJUTERIAS","Loja Tradicional de Brinquedos, com mais de 400 anos.","R. Sr. dos Passos, 279/283",-43.22,-23.22,4));
+			listLojas.add(new Lojas("ALFA 167","Loja Tradicional de Brinquedos, com mais de 100 anos.","R. da Alfândega, 240",-22.904127,-43.185734,1));
+			listLojas.add(new Lojas("HOT FASHION 373","Loja Tradicional de Brinquedos, com mais de 200 anos.","R. da Alfândega, 373",-22.904779,-43.187365,2));
+			listLojas.add(new Lojas("268 BIJUTERIAS","Loja Tradicional de Brinquedos, com mais de 300 anos.","R. Sr. dos Passos, 268",-22.9056,-43.187268,3));
+			listLojas.add(new Lojas("CAÇULA BIJUTERIAS","Loja Tradicional de Brinquedos, com mais de 400 anos.","R. Buenos Aires, 261",-22.904937,-43.184522,4));
 			break;
 			
 		case 3 :
 			
-			listLojas.add(new Lojas("BABADO DA FOLIA","Tudo para o seu carnaval 1 ","R. Sr. dos Passos, 279/283",-43.22,-23.22,1));
-			listLojas.add(new Lojas("CAÇULA CARNAVAL","Tudo para o seu carnaval 2 ","R. Sr. dos Passos, 279/283",-43.22,-23.22,2));
-			listLojas.add(new Lojas("CASA TURUNA","Tudo para o seu carnaval 3","R. Sr. dos Passos, 279/283",-43.22,-23.22,3));
-			listLojas.add(new Lojas("LOJAS E BIJ. SILMER","Tudo para o seu carnaval 4","R. Sr. dos Passos, 279/283",-43.22,-23.22,4));
+			listLojas.add(new Lojas("BABADO DA FOLIA","Tudo para o seu carnaval 1 ","R. Buenos Aires, 300",-22.905194,-43.185552,1));
+			listLojas.add(new Lojas("CAÇULA CARNAVAL","Tudo para o seu carnaval 2 ","R. Buenos Aires, 265, Slj",-22.904898,-43.184543,2));
+			listLojas.add(new Lojas("CASA TURUNA","Tudo para o seu carnaval 3","R. Buenos Aires, 265, Slj",-22.904898,-43.184543,3));
+			listLojas.add(new Lojas("LOJAS E BIJ. SILMER","Tudo para o seu carnaval 4",". Buenos Aires, 265, Slj",-22.904898,-43.184543,4));
 			break;
 			
 		case 4 :
 			
-			listLojas.add(new Lojas("DEDECO MODAS","Loja Tradicional de Brinquedos, com mais de 100 anos.","R. Sr. dos Passos, 279/283",-43.22,-23.22,1));
-			listLojas.add(new Lojas("ALDEIA DOS VENTOS","Loja Tradicional de Brinquedos, com mais de 200 anos.","R. Sr. dos Passos, 279/283",-43.22,-23.22,2));
-			listLojas.add(new Lojas("PRAIA DOS BAIXINHOS","Loja Tradicional de Brinquedos, com mais de 300 anos.","R. Sr. dos Passos, 279/283",-43.22,-23.22,3));
-			listLojas.add(new Lojas("CBK FASHION","Loja Tradicional de Brinquedos, com mais de 400 anos.","R. Sr. dos Passos, 279/283",-43.22,-23.22,4));
+			listLojas.add(new Lojas("DEDECO MODAS","Loja Tradicional de Brinquedos, com mais de 100 anos.","R. Buenos Aires, 265, Slj",-22.904898,-43.184543,1));
+			listLojas.add(new Lojas("ALDEIA DOS VENTOS","Loja Tradicional de Brinquedos, com mais de 200 anos.","R. Sr. dos Passos, 177",-22.904236,-43.184876,2));
+			listLojas.add(new Lojas("PRAIA DOS BAIXINHOS","Loja Tradicional de Brinquedos, com mais de 300 anos.","R. da Alfândega, 362",-22.904611,-43.187,3));
+			listLojas.add(new Lojas("CBK FASHION","Loja Tradicional de Brinquedos, com mais de 400 anos.","R. da Alfândega, 288",-22.904157,-43.184543,4));
 			break;
 		default:
 			break;
