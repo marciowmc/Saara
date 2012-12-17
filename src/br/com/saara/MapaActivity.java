257d450d73b49fd14bebdad61f0721902d960a34
Marcio William <marcio@easytaxi.com.br>
2012-12-17 15:03:36 -0200

    Atualizar Coordenadas

--- src/br/com/saara/MapaActivity.java
+++ src/br/com/saara/MapaActivity.java
@@ -36,6 +36,9 @@ public class MapaActivity extends MapActivity {
 		
 		Lojas loja = (Lojas) params.getSerializable("loja");
 		
+		latitude  = loja.getLatitude();
+		longitude = loja.getLongitude();
+		
 		TextView txtLoja = (TextView) findViewById(R.id.txtLoja);
 		TextView txtEnd  = (TextView) findViewById(R.id.txtEndereco);
 		
