5b005bdbf4a0be7e2558f9c5e40fa6d171a458cf
Marcio William <marcio@easytaxi.com.br>
2013-01-23 20:03:59 -0200

    Add drawables Add drawables/fundo

--- src/br/com/saara/CategoriasActivity.java
+++ src/br/com/saara/CategoriasActivity.java
@@ -28,49 +28,44 @@ public class CategoriasActivity extends Activity  {
 		setContentView(R.layout.categorias);
 		arrayCategorias = new ArrayList<Categorias>();
 		
-		arrayCategorias.add(new Categorias(R.drawable.armarinhos, getString(R.string.armarinhos),1, 10 , new int[]{78,205,196}));
-		arrayCategorias.add(new Categorias(R.drawable.brinquedos, getString(R.string.brinquedos),1, 23 , new int[]{255,162,0}));
-		arrayCategorias.add(new Categorias(R.drawable.carnaval, getString(R.string.carnaval),1, 17 , new int[]{82,221,92}));
-		arrayCategorias.add(new Categorias(R.drawable.decoracao, getString(R.string.decoracao),1, 17 , new int[]{242,29,65}));
-		arrayCategorias.add(new Categorias(R.drawable.estacionamento, getString(R.string.estacionamento),1, 10 , new int[]{255,198,0}));
-		arrayCategorias.add(new Categorias(R.drawable.bancos, getString(R.string.bancos),1, 23 , new int[]{96,2,72}));
-		arrayCategorias.add(new Categorias(R.drawable.bazares, getString(R.string.bazares),1, 17 , new int[]{189,235,7}));
+		arrayCategorias.add(new Categorias(R.drawable.alimentacao, getString(R.string.alimentacao),1, 10 , new int[]{78,205,196}));
+		arrayCategorias.add(new Categorias(R.drawable.armarinhos, getString(R.string.armarinhos),1, 10 , new int[]{255,162,0}));
+		arrayCategorias.add(new Categorias(R.drawable.art_esportivos, getString(R.string.esportivos),1, 23 , new int[]{82,221,92}));
+		arrayCategorias.add(new Categorias(R.drawable.art_festas, getString(R.string.festas),1, 17 , new int[]{242,29,65}));
+		arrayCategorias.add(new Categorias(R.drawable.artesanato, getString(R.string.artesanato),1, 17 ,new int[]{255,198,0}));
+		arrayCategorias.add(new Categorias(R.drawable.bancos, getString(R.string.bancos),1, 10 , new int[]{96,2,72}));
+		arrayCategorias.add(new Categorias(R.drawable.bazares, getString(R.string.bazares),1, 23 , new int[]{189,235,7}));
 		arrayCategorias.add(new Categorias(R.drawable.bijouterias, getString(R.string.bijuterias),1, 17 , new int[]{255,107,107 }));
+		arrayCategorias.add(new Categorias(R.drawable.brinquedos, getString(R.string.brinquedos),1, 17 , new int[]{63,176,148}));
+		arrayCategorias.add(new Categorias(R.drawable.calcados_bolsas_malas, getString(R.string.calcados),1, 17 , new int[]{242,100,61}));
 		
-		//arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.esportivos),2 , 10 , 0xFFA200));
-		/*arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.festas),3,  10 , "52DD5C"));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.artesanato),4 , 10 , "F21D41"));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.bancos),5 , 15, "FFC600"));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.bazares),6 ,19 , "601848"));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.bijuterias),7, 23, "BDEB07"));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.brinquedos),8, 9,"FF6B6B"));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.calcados),9));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.cama),10));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.carnaval),11));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.confeccao),12));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.decoracao),13));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.embalagens),14));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.drogaria),15));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.emporio),16));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.estacionamento),17));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.ferragens),18));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.flor),19));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.joias),20));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.bares),21));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.lingerie),22));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.malhas),23));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.moda),24));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.palhas),25));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.papelaria),26));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.perfumes),27));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.plasticos),28));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.presentes),29));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.feminina),30));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.infantil),31));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.silk),32));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.tecidos),33));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.vestuario),34));
-		arrayCategorias.add(new Categorias(R.drawable.bijuterias, getString(R.string.outros),35	));*/
+		arrayCategorias.add(new Categorias(R.drawable.cama_mesa_banho, getString(R.string.cama),1, 10 , new int[]{78,205,196}));
+		arrayCategorias.add(new Categorias(R.drawable.carnaval, getString(R.string.carnaval),1, 10 , new int[]{255,162,0}));
+		arrayCategorias.add(new Categorias(R.drawable.confeccao_tecidos, getString(R.string.confeccao),1, 23 , new int[]{82,221,92}));
+		arrayCategorias.add(new Categorias(R.drawable.decoracao, getString(R.string.decoracao),1, 17 , new int[]{242,29,65}));
+		arrayCategorias.add(new Categorias(R.drawable.descartavais_embalagens, getString(R.string.embalagens),1, 17 ,new int[]{255,198,0}));
+		arrayCategorias.add(new Categorias(R.drawable.drogarias, getString(R.string.drogaria),1, 10 , new int[]{96,2,72}));
+		arrayCategorias.add(new Categorias(R.drawable.emporio, getString(R.string.emporio),1, 23 , new int[]{189,235,7}));
+		arrayCategorias.add(new Categorias(R.drawable.estacionamento, getString(R.string.estacionamento),1, 17 , new int[]{255,107,107 }));
+		arrayCategorias.add(new Categorias(R.drawable.ferragens, getString(R.string.ferragens),1, 17 , new int[]{63,176,148}));
+		arrayCategorias.add(new Categorias(R.drawable.flores, getString(R.string.flor),1, 17 , new int[]{242,100,61}));
+		
+		arrayCategorias.add(new Categorias(R.drawable.relogios, getString(R.string.joias),1, 10 , new int[]{78,205,196}));
+		arrayCategorias.add(new Categorias(R.drawable.lingerie_erotica, getString(R.string.lingerie),1, 10 , new int[]{255,162,0}));
+		arrayCategorias.add(new Categorias(R.drawable.malhas_praia, getString(R.string.malhas),1, 23 , new int[]{82,221,92}));
+		arrayCategorias.add(new Categorias(R.drawable.moda_indiana, getString(R.string.moda),1, 17 , new int[]{242,29,65}));
+		arrayCategorias.add(new Categorias(R.drawable.palhas, getString(R.string.palhas),1, 17 ,new int[]{255,198,0}));
+		arrayCategorias.add(new Categorias(R.drawable.papelaria, getString(R.string.papelaria),1, 10 , new int[]{96,2,72}));
+		arrayCategorias.add(new Categorias(R.drawable.perfumes_cosmeticos, getString(R.string.perfumes),1, 23 , new int[]{189,235,7}));
+		arrayCategorias.add(new Categorias(R.drawable.plasticos, getString(R.string.plasticos),1, 17 , new int[]{255,107,107 }));
+		arrayCategorias.add(new Categorias(R.drawable.presentes, getString(R.string.presentes),1, 17 , new int[]{63,176,148}));
+		
+		arrayCategorias.add(new Categorias(R.drawable.roupa_infantil, getString(R.string.infantil),1, 10 , new int[]{78,205,196}));
+		arrayCategorias.add(new Categorias(R.drawable.silki, getString(R.string.silk),1, 10 , new int[]{255,162,0}));
+		arrayCategorias.add(new Categorias(R.drawable.vestuario_geral,getString(R.string.vestuario),1, 23 , new int[]{82,221,92}));
+		arrayCategorias.add(new Categorias(R.drawable.outros, getString(R.string.outros),1, 17 , new int[]{242,29,65}));
+		
+
 
 		CategoriaAdapter adapter = new CategoriaAdapter(this,arrayCategorias,R.layout.categoria_itens);
 		   
