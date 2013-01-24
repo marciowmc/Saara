64845415cbfca62478e74d6a740dbefe13746463
Marcio William <marcio@easytaxi.com.br>
2013-01-24 15:56:40 -0200

    Removido array de teste

--- src/br/com/saara/CategoriasActivity.java
+++ src/br/com/saara/CategoriasActivity.java
@@ -13,6 +13,7 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class CategoriasActivity extends Activity  {
 
@@ -31,20 +32,37 @@ public class CategoriasActivity extends Activity  {
 		btInfo      = (Button) findViewById(R.id.btInformacoes);
 		btFavoritos = (Button) findViewById(R.id.btFavoritos);
 		
+		btInfo.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(CategoriasActivity.this, "Clicou no botao info", Toast.LENGTH_LONG).show();
+			}
+		});
+		
+		btFavoritos.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(CategoriasActivity.this, "Clicou no botao favoritos", Toast.LENGTH_LONG).show();
+			}
+		});
 		
 		arrayCategorias = new ArrayList<Categorias>();
 		
 		// Monta lista
-		arrayCategorias.add(new Categorias(R.drawable.alimentacao, getString(R.string.alimentacao)       ,1, 17, new int[]{78,205,196} , new int[]{66,191,183} ));
-		arrayCategorias.add(new Categorias(R.drawable.armarinhos, getString(R.string.armarinhos)         ,2, 3 , new int[]{255,162,0}  , new int[]{235,149,2}  ));
-		arrayCategorias.add(new Categorias(R.drawable.artesanato, getString(R.string.artesanato)         ,3, 8 , new int[]{82,221,92}  , new int[]{68,207,78}  ));
-		arrayCategorias.add(new Categorias(R.drawable.art_festas, getString(R.string.festas)             ,4, 12, new int[]{242,29,65}  , new int[]{228,22,58}  ));
-		arrayCategorias.add(new Categorias(R.drawable.art_esportivos, getString(R.string.esportivos)     ,5, 1 , new int[]{255,198,0}  , new int[]{242,188,0}  ));
-		arrayCategorias.add(new Categorias(R.drawable.bancos, getString(R.string.bancos)                 ,6, 5 , new int[]{96,2,72}    , new int[]{76,15,56}   ));
-		arrayCategorias.add(new Categorias(R.drawable.bazares, getString(R.string.bazares)               ,7, 5 , new int[]{189,235,7}  , new int[]{180,224,3}  ));
-		arrayCategorias.add(new Categorias(R.drawable.bijouterias, getString(R.string.bijuterias)        ,8, 17, new int[]{255,107,107}, new int[]{241,97,97}  ));
-		arrayCategorias.add(new Categorias(R.drawable.brinquedos, getString(R.string.brinquedos)         ,9, 4 , new int[]{63,176,148} , new int[]{62,166,141} ));
-		arrayCategorias.add(new Categorias(R.drawable.calcados_bolsas_malas, getString(R.string.calcados),10,8 , new int[]{242,100,61} , new int[]{227,88,49}  ));
+		arrayCategorias.add(new Categorias(R.drawable.alimentacao, getString(R.string.alimentacao)       	 ,1, 17, new int[]{78,205,196} , new int[]{66,191,183} ));
+		arrayCategorias.add(new Categorias(R.drawable.armarinhos, getString(R.string.armarinhos)         	 ,2, 3 , new int[]{255,162,0}  , new int[]{235,149,2}  ));
+		arrayCategorias.add(new Categorias(R.drawable.artesanato, getString(R.string.artesanato)        	 ,3, 8 , new int[]{82,221,92}  , new int[]{68,207,78}  ));
+		arrayCategorias.add(new Categorias(R.drawable.art_festas, getString(R.string.festas)             	 ,4, 12, new int[]{242,29,65}  , new int[]{228,22,58}  ));
+		arrayCategorias.add(new Categorias(R.drawable.art_esportivos, getString(R.string.esportivos)     	 ,5, 1 , new int[]{255,198,0}  , new int[]{242,188,0}  ));
+		arrayCategorias.add(new Categorias(R.drawable.bancos, getString(R.string.bancos)                 	 ,6, 5 , new int[]{96,2,72}    , new int[]{76,15,56}   ));
+		arrayCategorias.add(new Categorias(R.drawable.bazares, getString(R.string.bazares)               	 ,7, 5 , new int[]{189,235,7}  , new int[]{180,224,3}  ));
+		arrayCategorias.add(new Categorias(R.drawable.bijouterias, getString(R.string.bijuterias)       	 ,8, 17, new int[]{255,107,107}, new int[]{241,97,97}  ));
+		arrayCategorias.add(new Categorias(R.drawable.brinquedos, getString(R.string.brinquedos)         	 ,9, 4 , new int[]{63,176,148} , new int[]{62,166,141} ));
+		arrayCategorias.add(new Categorias(R.drawable.calcados_bolsas_malas, getString(R.string.calcados)	 ,10,8 , new int[]{242,100,61} , new int[]{227,88,49}  ));
 		
 		arrayCategorias.add(new Categorias(R.drawable.cama_mesa_banho, getString(R.string.cama)              ,11, 8 , new int[]{78,205,196} , new int[]{66,191,183}));
 		arrayCategorias.add(new Categorias(R.drawable.carnaval, getString(R.string.carnaval)                 ,12, 5 , new int[]{255,162,0}  , new int[]{235,149,2}));
@@ -57,22 +75,21 @@ public class CategoriasActivity extends Activity  {
 		arrayCategorias.add(new Categorias(R.drawable.ferragens, getString(R.string.ferragens)               ,19, 5 , new int[]{63,176,148} , new int[]{62,166,141}));
 		arrayCategorias.add(new Categorias(R.drawable.flores, getString(R.string.flor)                       ,20, 2 , new int[]{242,100,61} , new int[]{227,88,49}));
 		
-		arrayCategorias.add(new Categorias(R.drawable.relogios, getString(R.string.joias)              ,21, 10 , new int[]{78,205,196} , new int[]{66,191,183} ));
-		arrayCategorias.add(new Categorias(R.drawable.lingerie_erotica, getString(R.string.lingerie)   ,22, 10 , new int[]{255,162,0}  , new int[]{235,149,2}));
-		arrayCategorias.add(new Categorias(R.drawable.malhas_praia, getString(R.string.malhas)         ,23, 23 , new int[]{82,221,92}  , new int[]{68,207,78}));
-		arrayCategorias.add(new Categorias(R.drawable.moda_indiana, getString(R.string.moda)           ,24, 17 , new int[]{242,29,65}  , new int[]{228,22,58}));
-		arrayCategorias.add(new Categorias(R.drawable.palhas, getString(R.string.palhas)               ,25, 17 , new int[]{255,198,0}  , new int[]{242,188,0}));
-		arrayCategorias.add(new Categorias(R.drawable.papelaria, getString(R.string.papelaria)         ,26, 10 , new int[]{96,2,72}    , new int[]{76,15,56}));
-		arrayCategorias.add(new Categorias(R.drawable.perfumes_cosmeticos, getString(R.string.perfumes),27, 23 , new int[]{189,235,7}  , new int[]{180,224,3}));
-		arrayCategorias.add(new Categorias(R.drawable.plasticos, getString(R.string.plasticos)         ,28, 17 , new int[]{255,107,107}, new int[]{241,97,97}));
-		arrayCategorias.add(new Categorias(R.drawable.presentes, getString(R.string.presentes)         ,29, 17 , new int[]{63,176,148} , new int[]{62,166,141}));
-		arrayCategorias.add(new Categorias(R.drawable.roupa_feminina, getString(R.string.feminina)     ,20, 2 , new int[]{242,100,61} , new int[]{227,88,49}));
+		arrayCategorias.add(new Categorias(R.drawable.relogios, getString(R.string.joias)              		 ,21, 10 , new int[]{78,205,196} , new int[]{66,191,183} ));
+		arrayCategorias.add(new Categorias(R.drawable.lingerie_erotica, getString(R.string.lingerie)   		 ,22, 10 , new int[]{255,162,0}  , new int[]{235,149,2}));
+		arrayCategorias.add(new Categorias(R.drawable.malhas_praia, getString(R.string.malhas)         		 ,23, 23 , new int[]{82,221,92}  , new int[]{68,207,78}));
+		arrayCategorias.add(new Categorias(R.drawable.moda_indiana, getString(R.string.moda)           		 ,24, 17 , new int[]{242,29,65}  , new int[]{228,22,58}));
+		arrayCategorias.add(new Categorias(R.drawable.palhas, getString(R.string.palhas)               		 ,26, 17 , new int[]{255,198,0}  , new int[]{242,188,0}));
+		arrayCategorias.add(new Categorias(R.drawable.papelaria, getString(R.string.papelaria)         		 ,27, 10 , new int[]{96,2,72}    , new int[]{76,15,56}));
+		arrayCategorias.add(new Categorias(R.drawable.perfumes_cosmeticos, getString(R.string.perfumes)		 ,28, 23 , new int[]{189,235,7}  , new int[]{180,224,3}));
+		arrayCategorias.add(new Categorias(R.drawable.plasticos, getString(R.string.plasticos)         		 ,29, 17 , new int[]{255,107,107}, new int[]{241,97,97}));
+		arrayCategorias.add(new Categorias(R.drawable.presentes, getString(R.string.presentes)         		 ,30, 17 , new int[]{63,176,148} , new int[]{62,166,141}));
+		arrayCategorias.add(new Categorias(R.drawable.roupa_feminina, getString(R.string.feminina)     		 ,31, 2 , new int[]{242,100,61} , new int[]{227,88,49}));
 		
-		arrayCategorias.add(new Categorias(R.drawable.roupa_infantil, getString(R.string.infantil)     ,1, 17, new int[]{78,205,196} , new int[]{66,191,183} ));
-		arrayCategorias.add(new Categorias(R.drawable.silki, getString(R.string.silk)                  ,2, 3 , new int[]{255,162,0}  , new int[]{235,149,2}  ));
-		arrayCategorias.add(new Categorias(R.drawable.vestuario_geral, getString(R.string.vestuario)   ,3, 8 , new int[]{82,221,92}  , new int[]{68,207,78}  ));
-		arrayCategorias.add(new Categorias(R.drawable.outros, getString(R.string.outros)               ,4, 12, new int[]{242,29,65}  , new int[]{228,22,58}  ));
-		
+		arrayCategorias.add(new Categorias(R.drawable.roupa_infantil, getString(R.string.infantil)     		 ,32, 17, new int[]{78,205,196} , new int[]{66,191,183} ));
+		arrayCategorias.add(new Categorias(R.drawable.silki, getString(R.string.silk)                  		 ,33, 3 , new int[]{255,162,0}  , new int[]{235,149,2}  ));
+		arrayCategorias.add(new Categorias(R.drawable.vestuario_geral, getString(R.string.vestuario)   		 ,34, 8 , new int[]{82,221,92}  , new int[]{68,207,78}  ));
+		arrayCategorias.add(new Categorias(R.drawable.outros, getString(R.string.outros)               		 ,25, 12, new int[]{242,29,65}  , new int[]{228,22,58}  ));
 
 		CategoriaAdapter adapter = new CategoriaAdapter(this,arrayCategorias,R.layout.categoria_itens);
 		   
@@ -87,7 +104,7 @@ public class CategoriasActivity extends Activity  {
 				   Intent intent = new Intent();
 				   intent.setClass(CategoriasActivity.this, LojasActivity.class);
 				   Log.i("Local", "Categorias id : "+ arrayCategorias.get(position).getIdCategoria());
-				   intent.putExtra("id_categoria", arrayCategorias.get(position).getIdCategoria());
+				   intent.putExtra("categoria",arrayCategorias.get(position));	
 				   intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 				   intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 				   startActivity(intent);
--- src/br/com/saara/LojasActivity.java
+++ src/br/com/saara/LojasActivity.java
@@ -2,6 +2,7 @@ package br.com.saara;
 
 import java.util.ArrayList;
 
+import beans.Categorias;
 import beans.Lojas;
 import adapters.LojaAdapter;
 import android.app.Activity;
@@ -30,45 +31,11 @@ public class LojasActivity extends Activity {
 		listLojas = new ArrayList<Lojas>();
 		
 		Bundle params = getIntent().getExtras();
-		int id_categoria = params.getInt("id_categoria");
+		Categorias categoria = (Categorias) params.getSerializable("categoria");
 		
-		Log.i("Local","Categoria : " +id_categoria);
+		Log.i("Local","Categoria : " +categoria.getIcon());
 		
-		switch (id_categoria) {
-		case 2:
-			listLojas.add(new Lojas("ARMARINHO DELMAR","Loja Tradicional de Brinquedos, com mais de 100 anos.","R. Sr. dos Passos, 279/283",-22.905204,-43.186828,1));
-			listLojas.add(new Lojas("BRINK MANIA","Loja Tradicional de Brinquedos, com mais de 200 anos.","R. da Alfândega, 314",-22.905204,-43.186828,2));
-			listLojas.add(new Lojas("LOJAS SIMÕES","Loja Tradicional de Brinquedos, com mais de 300 anos.","R. da Alfândega, 240",-22.904127,-43.185734,3));
-			listLojas.add(new Lojas("FESTA DA CIDADE - PRESENTES","Loja Tradicional de Brinquedos, com mais de 400 anos.","R. da Alfândega, 240",-22.904127,-43.185734,4));
-			break;
-
-		case 1 :
 			
-			listLojas.add(new Lojas("ALFA 167","Loja Tradicional de Brinquedos, com mais de 100 anos.","R. da Alfândega, 240",-22.904127,-43.185734,1));
-			listLojas.add(new Lojas("HOT FASHION 373","Loja Tradicional de Brinquedos, com mais de 200 anos.","R. da Alfândega, 373",-22.904779,-43.187365,2));
-			listLojas.add(new Lojas("268 BIJUTERIAS","Loja Tradicional de Brinquedos, com mais de 300 anos.","R. Sr. dos Passos, 268",-22.9056,-43.187268,3));
-			listLojas.add(new Lojas("CAÇULA BIJUTERIAS","Loja Tradicional de Brinquedos, com mais de 400 anos.","R. Buenos Aires, 261",-22.904937,-43.184522,4));
-			break;
-			
-		case 3 :
-			
-			listLojas.add(new Lojas("BABADO DA FOLIA","Tudo para o seu carnaval 1 ","R. Buenos Aires, 300",-22.905194,-43.185552,1));
-			listLojas.add(new Lojas("CAÇULA CARNAVAL","Tudo para o seu carnaval 2 ","R. Buenos Aires, 265, Slj",-22.904898,-43.184543,2));
-			listLojas.add(new Lojas("CASA TURUNA","Tudo para o seu carnaval 3","R. Buenos Aires, 265, Slj",-22.904898,-43.184543,3));
-			listLojas.add(new Lojas("LOJAS E BIJ. SILMER","Tudo para o seu carnaval 4",". Buenos Aires, 265, Slj",-22.904898,-43.184543,4));
-			break;
-			
-		case 4 :
-			
-			listLojas.add(new Lojas("DEDECO MODAS","Loja Tradicional de Brinquedos, com mais de 100 anos.","R. Buenos Aires, 265, Slj",-22.904898,-43.184543,1));
-			listLojas.add(new Lojas("ALDEIA DOS VENTOS","Loja Tradicional de Brinquedos, com mais de 200 anos.","R. Sr. dos Passos, 177",-22.904236,-43.184876,2));
-			listLojas.add(new Lojas("PRAIA DOS BAIXINHOS","Loja Tradicional de Brinquedos, com mais de 300 anos.","R. da Alfândega, 362",-22.904611,-43.187,3));
-			listLojas.add(new Lojas("CBK FASHION","Loja Tradicional de Brinquedos, com mais de 400 anos.","R. da Alfândega, 288",-22.904157,-43.184543,4));
-			break;
-		default:
-			break;
-		}
-		
 		 LojaAdapter adapter = new LojaAdapter(this,listLojas,R.layout.loja_itens);
 		   
 	       listView.setAdapter(adapter);
